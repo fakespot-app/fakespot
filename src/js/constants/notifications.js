76963545e40e1b1eb49b9734407eb64f661e4bdd
Bartosz Legięć <bartosz@legiec.eu>
2018-03-20 13:14:18 +0100

    Revert old lifeline times

--- src/js/constants/notifications.js
+++ src/js/constants/notifications.js
@@ -1,4 +1,2 @@
-// export const LIFE_LINE_1_TIME = 60; // time in seconds
-// export const LIFE_LINE_2_TIME = 120; // time in seconds
-export const LIFE_LINE_1_TIME = 0; // time in seconds
-export const LIFE_LINE_2_TIME = 0; // time in seconds
+export const LIFE_LINE_1_TIME = 60; // time in seconds
+export const LIFE_LINE_2_TIME = 120; // time in seconds
