790075244ba9627c2ffff75deccd5a0680565916
Bartosz Legięć <bartosz@legiec.eu>
2018-05-10 17:10:10 +0200

    Change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 (
   cd "$DIR/.." # Go to project dir.
 
-  echo $SSH_USERNAME
-
   ssh $SSH_USERNAME@$SSH_HOSTNAME -o StrictHostKeyChecking=no <<-EOF
     cd $SSH_PROJECT_FOLDER
     git pull
-    docker-compose pull
     docker stack rm $CIRCLE_PROJECT_REPONAME
     sleep 10
     docker network ls
